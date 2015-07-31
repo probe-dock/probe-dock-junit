6ad739c1697097af0c3abd9b06c7b1268b8e6196
Laurent Prevost <laurent.prevost@probedock.io>
2015-07-31 16:08:41 +0200

    Adapted from latest changes in the lib

--- src/main/java/io/probedock/client/junit/AbstractProbeListener.java
+++ src/main/java/io/probedock/client/junit/AbstractProbeListener.java
@@ -4,9 +4,8 @@ import io.probedock.client.annotations.ProbeTest;
 import io.probedock.client.annotations.ProbeTestClass;
 import io.probedock.client.common.config.Configuration;
 import io.probedock.client.common.model.v1.*;
-import io.probedock.client.common.utils.FingerprintGenerator;
 import io.probedock.client.common.utils.Inflector;
-import io.probedock.client.utils.CollectionHelper;
+import io.probedock.client.common.utils.TestResultDataUtils;
 
 import java.util.*;
 import java.util.logging.Logger;
@@ -21,268 +20,194 @@ import org.junit.runner.notification.RunListener;
  * @author Laurent Prevost <laurent.prevost@probedock.io>
  */
 public abstract class AbstractProbeListener extends RunListener {
-	private static final Logger LOGGER = Logger.getLogger(AbstractProbeListener.class.getCanonicalName());
-	
-	/**
-	 * Probe Dock configuration
-	 */
-	protected static final Configuration configuration = Configuration.getInstance();
+    private static final Logger LOGGER = Logger.getLogger(AbstractProbeListener.class.getCanonicalName());
 
-	/**
-	 * Default category when none is specified
-	 */
-	private static final String DEFAULT_CATEGORY = "unit";
+    /**
+     * Probe Dock configuration
+     */
+    protected static final Configuration configuration = Configuration.getInstance();
 
-	/**
-	 * Store the date when the run started
-	 */
-	protected Long runStartedDate;
+    /**
+     * Default category when none is specified
+     */
+    protected static final String DEFAULT_CATEGORY = "unit";
 
-	/**
-	 * Store the start date of a test to measure the approximative execution
-	 * time of a each test.
-	 */
-	protected Map<String, Long> testStartDates = new HashMap<>();
+    /**
+     * Store the date when the run started
+     */
+    protected Long runStartedDate;
 
-	/**
-	 * Define if all the stack traces must be printed/written
-	 */
-	private boolean fullStackTraces = true;
-	
-	/**
-	 * Define the category of the test that are running. Will
-	 * be use donly if no other is specified.
-	 */
-	private String category;
-	
-	/**
-	 * Constructor
-	 */
-	public AbstractProbeListener() {
-	}
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param category The test category
-	 */
-	public AbstractProbeListener(String category) {
-		this();
-		this.category = category;
-	}
+    /**
+     * Store the start date of a test to measure the approximative execution time of a each test.
+     */
+    protected Map<String, Long> testStartDates = new HashMap<>();
 
-	@Override
-	public void testStarted(Description description) throws Exception {
-		super.testStarted(description);
-	}
+    /**
+     * Define if all the stack traces must be printed/written
+     */
+    private boolean fullStackTraces = true;
 
-	@Override
-	public void testFinished(Description description) throws Exception {
-		super.testFinished(description);
-	}
-	
-	/**
-	 * @param fullStackTraces The full stack trace mode
-	 */
-	public void setFullStackTraces(Boolean fullStackTraces) {
-		this.fullStackTraces = fullStackTraces;
-	}
-	
-	@Override
-	public void testRunStarted(Description description) throws Exception {
-		super.testRunStarted(description);
-		runStartedDate = System.currentTimeMillis();
-	}
-	
-	/**
-	 * Try to retrieve the {@link ProbeTest} annotation of the test method
-	 * 
-	 * @param description The representation of the test
-	 * @return The annotation found, or null if not found
-	 * @throws NoSuchMethodException 
-	 */
-	protected ProbeTest getMethodAnnotation(Description description) throws NoSuchMethodException {
-		String methodName = description.getMethodName();
+    /**
+     * Define the category of the test that are running. Will be use donly if no other is specified.
+     */
+    protected String category;
 
-		if (methodName.contains("[")) {
-			methodName = methodName.replaceAll("\\[.*\\]", "");
-		}
+    /**
+     * Constructor
+     */
+    public AbstractProbeListener() {
+    }
 
-		return description.getTestClass().getMethod(methodName).getAnnotation(ProbeTest.class);
-	}
-	
-	/**
-	 * Try to retrieve the {@link ProbeTestClass} annotation of the test class
-	 * 
-	 * @param description The representation of the test
-	 * @return The annotation found, or null if not found
-	 */
-	protected ProbeTestClass getClassAnnotation(Description description) {
-		return description.getTestClass().getAnnotation(ProbeTestClass.class);
-	}
+    /**
+     * Constructor
+     *
+     * @param category The test category
+     */
+    public AbstractProbeListener(String category) {
+        this();
+        this.category = category;
+    }
 
-	/**
-	 * Create a test based on the different information gathered from class, method and description
-	 *
-	 * @param fingerprint The fingerprint of the test
-	 * @param description jUnit test description
-	 * @param mAnnotation Method annotation
-	 * @param cAnnotation Class annotation
-	 * @param passed Test passing or not
-	 * @param message Message associated to the test result
-	 * @return The test created from all the data available
-	 */
-	protected TestResult createTestResult(String fingerprint, Description description, ProbeTest mAnnotation, ProbeTestClass cAnnotation, boolean passed, String message) {
-		TestResult result = ModelFactory.createTestResult(
-			getKey(mAnnotation),
-			fingerprint,
-			Inflector.forgeName(description.getTestClass(), description.getMethodName(), mAnnotation),
-			getCategory(cAnnotation, mAnnotation),
-			System.currentTimeMillis() - testStartDates.get(fingerprint),
-			message,
-			passed,
-			isActive(mAnnotation),
-			getContributors(mAnnotation, cAnnotation),
-			getTags(mAnnotation, cAnnotation),
-			getTickets(mAnnotation, cAnnotation),
-			null
-		);
+    @Override
+    public void testStarted(Description description) throws Exception {
+        super.testStarted(description);
+    }
 
-		ModelFactory.enrichTestResult(result, description.getTestClass().getPackage().getName(), description.getTestClass().getName(), description.getMethodName());
+    @Override
+    public void testFinished(Description description) throws Exception {
+        super.testFinished(description);
+    }
 
-		return result;
-	}
+    /**
+     * @param fullStackTraces The full stack trace mode
+     */
+    public void setFullStackTraces(Boolean fullStackTraces) {
+        this.fullStackTraces = fullStackTraces;
+    }
 
-	/**
-	 * Retrieve the tags from the annotations
-	 *
-	 * @param annotation The annotation
-	 * @return The key or null if there is no key
-	 */
-	private String getKey(ProbeTest annotation) {
-		return annotation != null && annotation.key() != null && !annotation.key().isEmpty() ? annotation.key() : null;
-	}
+    @Override
+    public void testRunStarted(Description description) throws Exception {
+        super.testRunStarted(description);
+        runStartedDate = System.currentTimeMillis();
+    }
 
-	private Boolean isActive(ProbeTest annotation) {
-		return annotation != null ? annotation.active() : null;
-	}
-	
-	/**
-	 * Retrieve the category to apply to the test
-	 * @param classAnnotation The roxable class annotation to get the override category
-	 * @param methodAnnotation The roxable annotation to get the override category
-	 * @return The category found
-	 */
-	protected String getCategory(ProbeTestClass classAnnotation, ProbeTest methodAnnotation) {
-		if (methodAnnotation != null && methodAnnotation.category() != null && !methodAnnotation.category().isEmpty()) {
-			return methodAnnotation.category();
-		}
-		else if (classAnnotation != null && classAnnotation.category() != null && !classAnnotation.category().isEmpty()) {
-			return classAnnotation.category();
-		}
-		else if (configuration.getCategory() != null && !configuration.getCategory().isEmpty()) {
-			return configuration.getCategory();
-		}
-		else if (category != null) {
-			return category;
-		}
-		else {
-			return DEFAULT_CATEGORY;
-		}
-	}
+    /**
+     * Try to retrieve the {@link ProbeTest} annotation of the test method
+     *
+     * @param description The representation of the test
+     * @return The annotation found, or null if not found
+     * @throws NoSuchMethodException
+     */
+    protected ProbeTest getMethodAnnotation(Description description) throws NoSuchMethodException {
+        String methodName = description.getMethodName();
 
-	/**
-	 * Compute the list of contributors associated for a test
-	 *
-	 * @param methodAnnotation The method annotation to get info
-	 * @param classAnnotation The class annotation to get info
-	 * @return The contributors associated to the test
-	 */
-	private Set<String> getContributors(ProbeTest methodAnnotation, ProbeTestClass classAnnotation) {
-		return CollectionHelper.getContributors(configuration.getTags(), methodAnnotation, classAnnotation);
-	}
+        if (methodName.contains("[")) {
+            methodName = methodName.replaceAll("\\[.*\\]", "");
+        }
 
-	/**
-	 * Compute the list of tags associated for a test
-	 * 
-	 * @param methodAnnotation The method annotation to get info
-	 * @param classAnnotation The class annotation to get info
-	 * @return The tags associated to the test
-	 */
-	private Set<String> getTags(ProbeTest methodAnnotation, ProbeTestClass classAnnotation) {
-		return CollectionHelper.getTags(configuration.getTags(), methodAnnotation, classAnnotation);
-	}
+        return description.getTestClass().getMethod(methodName).getAnnotation(ProbeTest.class);
+    }
 
-	/**
-	 * Compute the list of tickets associated for a test
-	 * 
-	 * @param methodAnnotation The method annotation to get info
-	 * @param classAnnotation The class annotation to get info
-	 * @return The tickets associated to the test
-	 */
-	private Set<String> getTickets(ProbeTest methodAnnotation, ProbeTestClass classAnnotation) {
-		return CollectionHelper.getTickets(configuration.getTickets(), methodAnnotation, classAnnotation);
-	}
-	
-	/**
-	 * Build a stack trace string
-	 * 
-	 * @param failure The failure to get the exceptions and so on
-	 * @return The stack trace stringified
-	 */
-	protected String createAndlogStackTrace(Failure failure) {
-		StringBuilder sb = new StringBuilder();
+    /**
+     * Try to retrieve the {@link ProbeTestClass} annotation of the test class
+     *
+     * @param description The representation of the test
+     * @return The annotation found, or null if not found
+     */
+    protected ProbeTestClass getClassAnnotation(Description description) {
+        return description.getTestClass().getAnnotation(ProbeTestClass.class);
+    }
 
-		if (failure.getMessage() != null && !failure.getMessage().isEmpty()) {
-			sb.append("Failure message: ").append(failure.getMessage());
-		}
-		
-		if (failure.getException() != null) {
-			sb.append("\n\n");
-			sb.append(failure.getException().getClass().getCanonicalName()).append(": ").append(failure.getMessage()).append("\n");
-		
-			for (StackTraceElement ste : failure.getException().getStackTrace()) {
-				sb.append("\tat ").append(ste.getClassName()).append(".").append(ste.getMethodName()).
-					append("(").append(ste.getFileName()).append(":").append(ste.getLineNumber()).append(")\n");
-				
-				if (!fullStackTraces && ste.getClassName().equals(failure.getDescription().getClassName())) {
-					sb.append("\t...\n");
-					break;
-				}
-			}
+    /**
+     * Create a test based on the different information gathered from class, method and description
+     *
+     * @param fingerprint The fingerprint of the test
+     * @param description jUnit test description
+     * @param mAnnotation Method annotation
+     * @param cAnnotation Class annotation
+     * @param passed Test passing or not
+     * @param message Message associated to the test result
+     * @return The test created from all the data available
+     */
+    protected TestResult createTestResult(String fingerprint, Description description, ProbeTest mAnnotation, ProbeTestClass cAnnotation, boolean passed, String message) {
+        TestResult result = ModelFactory.createTestResult(
+            TestResultDataUtils.getKey(mAnnotation),
+            fingerprint,
+            Inflector.forgeName(description.getTestClass(), description.getMethodName(), mAnnotation),
+            TestResultDataUtils.getCategory(configuration, cAnnotation, mAnnotation, getCategory()),
+            System.currentTimeMillis() - testStartDates.get(fingerprint),
+            message,
+            passed,
+            TestResultDataUtils.isActive(mAnnotation),
+            TestResultDataUtils.getContributors(configuration, cAnnotation, mAnnotation),
+            TestResultDataUtils.getTags(configuration, cAnnotation, mAnnotation),
+            TestResultDataUtils.getTickets(configuration, cAnnotation, mAnnotation),
+            null
+        );
 
-			if (fullStackTraces && failure.getException().getCause() != null) {
-				sb.append("Cause: ").append(failure.getException().getCause().getMessage()).append("\n");
-				
-				for (StackTraceElement ste : failure.getException().getCause().getStackTrace()) {
-					sb.append("\tat ").append(ste.getClassName()).append(".").append(ste.getMethodName()).
-						append("(").append(ste.getFileName()).append(":").append(ste.getLineNumber()).append(")\n");
-				}
-			}
-			
-			LOGGER.info("\n" + failure.getTestHeader() + "\n" + sb.toString());
-		}
-		
-		return sb.toString();
-	}
-	
-	/**
-	 * Build the technical name
-	 * 
-	 * @param description The description to retrieve the unique name of a test
-	 * @return The technical name
-	 */
-	protected String getTechnicalName(Description description) {
-		return description.getClassName() + "." + description.getMethodName();
-	}
+        ModelFactory.enrichTestResult(result, description.getTestClass().getPackage().getName(), description.getTestClass().getName(), description.getMethodName());
 
-	/**
-	 * Calculate a fingerpring for a test
-	 *
-	 * @param description The description to retrieve package, class and method
-	 * @return The fingerprint calculated
-	 */
-	protected String getFingerprint(Description description) {
-		return FingerprintGenerator.fingerprint(description.getTestClass(), description.getMethodName());
-	}
+        return result;
+    }
+
+    /**
+     * Build a stack trace string
+     *
+     * @param failure The failure to get the exceptions and so on
+     * @return The stack trace stringified
+     */
+    protected String createAndlogStackTrace(Failure failure) {
+        StringBuilder sb = new StringBuilder();
+
+        if (failure.getMessage() != null && !failure.getMessage().isEmpty()) {
+            sb.append("Failure message: ").append(failure.getMessage());
+        }
+
+        if (failure.getException() != null) {
+            sb.append("\n\n");
+            sb.append(failure.getException().getClass().getCanonicalName()).append(": ").append(failure.getMessage()).append("\n");
+
+            for (StackTraceElement ste : failure.getException().getStackTrace()) {
+                sb.append("\tat ").append(ste.getClassName()).append(".").append(ste.getMethodName()).
+                    append("(").append(ste.getFileName()).append(":").append(ste.getLineNumber()).append(")\n");
+
+                if (!fullStackTraces && ste.getClassName().equals(failure.getDescription().getClassName())) {
+                    sb.append("\t...\n");
+                    break;
+                }
+            }
+
+            if (fullStackTraces && failure.getException().getCause() != null) {
+                sb.append("Cause: ").append(failure.getException().getCause().getMessage()).append("\n");
+
+                for (StackTraceElement ste : failure.getException().getCause().getStackTrace()) {
+                    sb.append("\tat ").append(ste.getClassName()).append(".").append(ste.getMethodName()).
+                        append("(").append(ste.getFileName()).append(":").append(ste.getLineNumber()).append(")\n");
+                }
+            }
+
+            LOGGER.info("\n" + failure.getTestHeader() + "\n" + sb.toString());
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Retrieve the fingerprint of a test based on its description
+     *
+     * @param description The description
+     * @return The fingerprint
+     */
+    protected final String getFingerprint(Description description) {
+        return TestResultDataUtils.getFingerprint(description.getTestClass(), description.getMethodName());
+    }
+
+    /**
+     * Retrive the category
+     *
+     * @return The category
+     */
+    protected final String getCategory() {
+        return category != null && !category.isEmpty() ? category : DEFAULT_CATEGORY;
+    }
 }
--- src/main/java/io/probedock/client/junit/ProbeListener.java
+++ src/main/java/io/probedock/client/junit/ProbeListener.java
@@ -5,8 +5,10 @@ import io.probedock.client.annotations.ProbeTest;
 import io.probedock.client.annotations.ProbeTestClass;
 import io.probedock.client.common.config.Configuration;
 import io.probedock.client.common.model.v1.*;
+import io.probedock.client.common.utils.TestResultDataUtils;
 import io.probedock.client.core.connector.Connector;
 import io.probedock.client.core.storage.FileStore;
+
 import java.io.IOException;
 import java.util.*;
 import java.util.logging.Level;
@@ -22,165 +24,166 @@ import org.junit.runner.notification.Failure;
  * @author Laurent Prevost <laurent.prevost@probedock.io>
  */
 public class ProbeListener extends AbstractProbeListener {
-	private static final Logger LOGGER = Logger.getLogger(ProbeListener.class.getCanonicalName());
+    private static final Logger LOGGER = Logger.getLogger(ProbeListener.class.getCanonicalName());
 
-	/**
-	 * Probe Dock JUnit and version
-	 */
-	private static final String PROBE_DOCK_NAME = "JUnit";
-	private static final String PROBE_DOCK_VERSION = ResourceBundle.getBundle("version").getString("version");
+    /**
+     * Probe Dock JUnit and version
+     */
+    private static final String PROBE_DOCK_NAME = "Junit";
+    private static final String PROBE_DOCK_VERSION = ResourceBundle.getBundle("version").getString("version");
 
-	/**
-	 * The execution context
-	 */
-	protected Context context;
+    /**
+     * The execution context
+     */
+    protected Context context;
 
-	/**
-	 * The test run to publish
-	 */
-	protected TestRun testRun;
+    /**
+     * The test run to publish
+     */
+    protected TestRun testRun;
 
-	/**
-	 * Store the list of the tests executed
-	 */
-	private List<TestResult> results = new ArrayList<>();
+    /**
+     * Store the list of the tests executed
+     */
+    private List<TestResult> results = new ArrayList<>();
 
-	/**
-	 * Store the test that fail to handle correctly the difference between test failures and test
-	 * success in the testFinished method.
-	 */
-	private Set<String> testFailures = new HashSet<>();
+    /**
+     * Store the test that fail to handle correctly the difference between test failures and test success in the
+     * testFinished method.
+     */
+    private Set<String> testFailures = new HashSet<>();
 
-	/**
-	 * Constructor
-	 */
-	public ProbeListener() {}
+    /**
+     * Constructor
+     */
+    public ProbeListener() {
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param category The default category for the listener
-	 */
-	public ProbeListener(String category) {
-		super(category);
-	}
+    /**
+     * Constructor
+     *
+     * @param category The default category for the listener
+     */
+    public ProbeListener(String category) {
+        super(category);
+    }
 
-	@Override
-	public void testRunStarted(Description description) throws Exception {
-		super.testRunStarted(description);
+    @Override
+    public void testRunStarted(Description description) throws Exception {
+        super.testRunStarted(description);
 
-		// Ensure there is nothing to do when ROX is disabled
-		if (configuration.isDisabled()) {
-			return;
-		}
+        // Ensure there is nothing to do when ROX is disabled
+        if (configuration.isDisabled()) {
+            return;
+        }
 
-		context = ModelFactory.createContext();
-		Probe probe = ModelFactory.createProbe(PROBE_DOCK_NAME, PROBE_DOCK_VERSION);
+        context = ModelFactory.createContext();
+        Probe probe = ModelFactory.createProbe(PROBE_DOCK_NAME, PROBE_DOCK_VERSION);
 
-		testRun = ModelFactory.createTestRun(
-			Configuration.getInstance(),
-			context,
-			probe,
-			configuration.getProjectApiId(),
-			configuration.getProjectVersion(),
-			configuration.getPipeline(),
-			configuration.getStage(),
-			null,
-			null
-		);
-	}
+        testRun = ModelFactory.createTestRun(
+            Configuration.getInstance(),
+            context,
+            probe,
+            configuration.getProjectApiId(),
+            configuration.getProjectVersion(),
+            configuration.getPipeline(),
+            configuration.getStage(),
+            null,
+            null
+        );
+    }
 
-	@Override
-	public void testRunFinished(Result result) throws Exception {
-		// Ensure there is nothing to do when ROX is disabled
-		if (configuration.isDisabled()) {
-			return;
-		}
+    @Override
+    public void testRunFinished(Result result) throws Exception {
+        // Ensure there is nothing to do when ROX is disabled
+        if (configuration.isDisabled()) {
+            return;
+        }
 
-		if (!results.isEmpty()) {
-			ModelFactory.enrichContext(context);
-			long runEndedDate = System.currentTimeMillis();
-			testRun.setDuration(runEndedDate - runStartedDate);
-			testRun.addTestResults(results);
+        if (!results.isEmpty()) {
+            ModelFactory.enrichContext(context);
+            long runEndedDate = System.currentTimeMillis();
+            testRun.setDuration(runEndedDate - runStartedDate);
+            testRun.addTestResults(results);
 
-			try {
-				publishTestRun();
-			} catch (ProbeRuntimeException pre) {
-				LOGGER.log(Level.WARNING, "Could not publish or save test run", pre);
-			}
-		}
-	}
+            try {
+                publishTestRun();
+            } catch (ProbeRuntimeException pre) {
+                LOGGER.log(Level.WARNING, "Could not publish or save test run", pre);
+            }
+        }
+    }
 
-	/**
-	 * Publish the results to Probe Dock
-	 *
-	 * @throws IOException Any I/O error
-	 */
-	private void publishTestRun() throws IOException {
-		if (configuration.isPublish() || configuration.isSave()) {
-			if (configuration.isSave()) {
-				new FileStore(configuration).save(testRun);
-			}
+    /**
+     * Publish the results to Probe Dock
+     *
+     * @throws IOException Any I/O error
+     */
+    private void publishTestRun() throws IOException {
+        if (configuration.isPublish() || configuration.isSave()) {
+            if (configuration.isSave()) {
+                new FileStore(configuration).save(testRun);
+            }
 
-			if (configuration.isPublish()) {
-				new Connector(configuration).send(testRun);
-			}
-		}
-	}
+            if (configuration.isPublish()) {
+                new Connector(configuration).send(testRun);
+            }
+        }
+    }
 
-	@Override
-	public void testStarted(Description description) throws Exception {
-		super.testStarted(description);
+    @Override
+    public void testStarted(Description description) throws Exception {
+        super.testStarted(description);
 
-		// Ensure there is nothing to do when ROX is disabled
-		if (configuration.isDisabled()) {
-			return;
-		}
+        // Ensure there is nothing to do when ROX is disabled
+        if (configuration.isDisabled()) {
+            return;
+        }
 
-		testStartDates.put(getFingerprint(description), System.currentTimeMillis());
-	}
+        testStartDates.put(getFingerprint(description), System.currentTimeMillis());
+    }
 
-	@Override
-	public void testFinished(Description description) throws Exception {
-		super.testFinished(description);
+    @Override
+    public void testFinished(Description description) throws Exception {
+        super.testFinished(description);
 
-		// Ensure there is nothing to do when ROX is disabled
-		if (configuration.isDisabled()) {
-			return;
-		}
+        // Ensure there is nothing to do when ROX is disabled
+        if (configuration.isDisabled()) {
+            return;
+        }
 
-		ProbeTest mAnnotation = getMethodAnnotation(description);
-		ProbeTestClass cAnnotation = getClassAnnotation(description);
+        ProbeTest mAnnotation = getMethodAnnotation(description);
+        ProbeTestClass cAnnotation = getClassAnnotation(description);
 
-		String fingerprint = getFingerprint(description);
+        String fingerprint = getFingerprint(description);
 
-		if (!testFailures.contains(fingerprint)) {
-			// Create a test result
-			TestResult testResult = createTestResult(fingerprint, description, mAnnotation, cAnnotation, true, null);
+        if (!testFailures.contains(fingerprint)) {
+            // Create a test result
+            TestResult testResult = createTestResult(fingerprint, description, mAnnotation, cAnnotation, true, null);
 
-			results.add(testResult);
-		}
-	}
+            results.add(testResult);
+        }
+    }
 
-	@Override
-	public void testFailure(Failure failure) throws Exception {
-		super.testFailure(failure);
+    @Override
+    public void testFailure(Failure failure) throws Exception {
+        super.testFailure(failure);
 
-		// Ensure there is nothing to do when ROX is disabled
-		if (configuration.isDisabled()) {
-			return;
-		}
+        // Ensure there is nothing to do when ROX is disabled
+        if (configuration.isDisabled()) {
+            return;
+        }
 
-		ProbeTest mAnnotation = getMethodAnnotation(failure.getDescription());
-		ProbeTestClass cAnnotation = getClassAnnotation(failure.getDescription());
+        ProbeTest mAnnotation = getMethodAnnotation(failure.getDescription());
+        ProbeTestClass cAnnotation = getClassAnnotation(failure.getDescription());
 
-		String fingerprint = getFingerprint(failure.getDescription());
+        String fingerprint = getFingerprint(failure.getDescription());
 
-		testFailures.add(fingerprint);
+        testFailures.add(fingerprint);
 
-		// Create the test result
-		TestResult testResult = createTestResult(fingerprint, failure.getDescription(), mAnnotation, cAnnotation, false, createAndlogStackTrace(failure));
+        // Create the test result
+        TestResult testResult = createTestResult(fingerprint, failure.getDescription(), mAnnotation, cAnnotation, false, createAndlogStackTrace(failure));
 
-		results.add(testResult);
-	}
+        results.add(testResult);
+    }
 }
